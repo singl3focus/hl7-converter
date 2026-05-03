4a10078d655d724c800d5ba87f3b0278d32ffef7
Tursunov Imran <tursunov.imran@mail.ru>
2026-05-03 17:48:56 +0300

    chore: small cleanup before improvement plan continuation
    - rename config.s<cyrillic-c>hema.json.go -> config.schema.json.go
    
    - update CfgJSON to point at examples/config.json (deprecated, kept for compat)
    
    - replace broken struct-tag-comment on Modification.ComponentSeparator with a normal comment
    
    - move converter error vars and constructors from converter.go to errors.go
    
    - reword ErrOutputTagNotFound message and drop the clarify TODO
    
    - reword InsertRow/RemoveRowByIndex TODOs as explicit open questions

--- errors.go
+++ errors.go
@@ -68,3 +68,119 @@ var (
 	ErrUndefinedScannerFailure = errors.New("unknown scanner failure")
 	ErrParseFailure            = errors.New("parse failure")
 )
+
+// Package-level converting errors that are part of the public API.
+var (
+	FatalErrOfConverting     = errors.New("convert error: parse input message to struct has been unsuccesful")
+	ErrInvalidParseMsg       = errors.New("convert error: parse input message to struct has been unsuccesful")
+	ErrInputTagNotFound      = errors.New("convert error: input tag not found")
+	ErrOutputTagNotFound     = errors.New("convert error: input tag has no linked tag in output modification")
+	ErrUndefinedOption       = errors.New("convert error: undefined option by tag")
+	ErrUndefinedPositionTag  = errors.New("convert error: tag has position, but it's not found in tags")
+	ErrInvalidLink           = errors.New("convert error: invalid link")
+	ErrInvalidLinkElems      = errors.New("convert error: invalid link, some elems empty")
+	ErrWrongParamCount       = errors.New("convert error: you can use only one special symbol in field")
+	ErrEmptyDefaultValue     = errors.New("convert error: field has empty default value")
+	ErrTooBigIndex           = errors.New("convert error: index of output rows more than max index of input rows")
+	ErrWrongFieldsNumber     = errors.New("convert error: tag has invalid specified count of fields number")
+	ErrWrongComponentsNumber = errors.New("convert error: component not found but link is exist")
+	ErrWrongComponentLink    = errors.New("convert error: link component position more than max components count of input row with tag")
+)
+
+func NewFatalErrOfConverting(r any) error {
+	return &Error{
+		Err:            FatalErrOfConverting,
+		AdditionalInfo: fmt.Sprintf("recovered %+v", r),
+	}
+}
+
+func NewErrInvalidParseMsg(err string) error {
+	return &Error{
+		Err:            ErrInvalidParseMsg,
+		AdditionalInfo: fmt.Sprintf("system error %s", err),
+	}
+}
+
+func NewErrInputTagNotFound(row string) error {
+	return &Error{
+		Err:            ErrInputTagNotFound,
+		AdditionalInfo: fmt.Sprintf("row %s", row),
+	}
+}
+
+func NewErrOutputTagNotFound(tag string) error {
+	return &Error{
+		Err:            ErrOutputTagNotFound,
+		AdditionalInfo: fmt.Sprintf("input tag %s", tag),
+	}
+}
+
+func NewErrUndefinedOption(option, tag string) error {
+	return &Error{
+		Err:            ErrUndefinedOption,
+		AdditionalInfo: fmt.Sprintf("option %s tag %s, available %s", option, tag, supportedOptionsSummary()),
+	}
+}
+
+func NewErrUndefinedPositionTag(tag string) error {
+	return &Error{
+		Err:            ErrUndefinedPositionTag,
+		AdditionalInfo: fmt.Sprintf("tag %s", tag),
+	}
+}
+
+func NewErrInvalidLink(link string) error {
+	return &Error{
+		Err:            ErrInvalidLink,
+		AdditionalInfo: fmt.Sprintf("link %s", link),
+	}
+}
+
+func NewErrInvalidLinkElems(link string) error {
+	return &Error{
+		Err:            ErrInvalidLinkElems,
+		AdditionalInfo: fmt.Sprintf("link %s", link),
+	}
+}
+
+func NewErrWrongParamCount(field, param string) error {
+	return &Error{
+		Err:            ErrWrongParamCount,
+		AdditionalInfo: fmt.Sprintf("field %s param %s", field, param),
+	}
+}
+
+func NewErrEmptyDefaultValue(field string) error {
+	return &Error{
+		Err:            ErrEmptyDefaultValue,
+		AdditionalInfo: fmt.Sprintf("field %s", field),
+	}
+}
+
+func NewErrTooBigIndex(idx, maxIdx int) error {
+	return &Error{
+		Err:            ErrTooBigIndex,
+		AdditionalInfo: fmt.Sprintf("index %d maxIndex %d", idx, maxIdx),
+	}
+}
+
+func NewErrWrongFieldsNumber(tag string, tagstruct *Tag, currentFieldsNumb int) error {
+	return &Error{
+		Err:            ErrWrongFieldsNumber,
+		AdditionalInfo: fmt.Sprintf("tagName %s tagSturcture %+v current fields has %d", tag, tagstruct, currentFieldsNumb),
+	}
+}
+
+func NewErrWrongComponentsNumber(inputdata, link string) error {
+	return &Error{
+		Err:            ErrWrongComponentsNumber,
+		AdditionalInfo: fmt.Sprintf("line %s link %s", inputdata, link),
+	}
+}
+
+func NewErrWrongComponentLink(link string, compPos, compCount int, inputTag string) error {
+	return &Error{
+		Err:            ErrWrongComponentLink,
+		AdditionalInfo: fmt.Sprintf("tag %s link %s componentPosition %d componentCount %d", inputTag, link, compPos, compCount),
+	}
+}
